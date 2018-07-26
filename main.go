f36d3cfcd9a783f3311f0afd37b822d333d92471
Juan Pablo <juanpgenovese@gmail.com>
2018-07-26 19:28:16 +0200

    Add command line parser

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/url"
+	"strings"
 
+	"github.com/devfacet/gocmd"
 	"github.com/gocolly/colly"
 	"github.com/kr/pretty"
 )
@@ -16,14 +18,16 @@ type sub struct {
 
 var subs = []sub{}
 
+var debug = false
+
 //c.Visit("https://subdivx.com/index.php?buscar=smallville+s01e06&accion=5&masdesc=&subtitulos=1&realiza_b=1")
-func generateURL() string {
+func generateURL(args string) string {
 	u := url.URL{}
 	u.Scheme = "https"
 	u.Host = "subdivx.com"
 	u.Path = "index.php"
 	q := u.Query()
-	q.Set("buscar", "smallville s01e06")
+	q.Set("buscar", args)
 	q.Set("accion", "5")
 	q.Set("masdesc", "")
 	q.Set("subtitulos", "1")
@@ -33,7 +37,44 @@ func generateURL() string {
 }
 
 func main() {
-	generateURL()
+
+	flags := struct {
+		Help    bool `short:"h" long:"help" description:"Display usage" global:"true"`
+		Version bool `short:"v" long:"version" description:"Display version"`
+		Debug   bool `short:"b" long:"debub" description:"Enable debugging"`
+		Grab    struct {
+			Settings bool `settings:"true" allow-unknown-arg:"true"`
+		} `command:"grab" description:"Grabs a subtitle based on the provided arguments"`
+	}{}
+
+	cmd, err := gocmd.New(gocmd.Options{
+		Name:        "subgrab",
+		Version:     "0.0.1",
+		Description: "A smart (well, not su much right now) subtitle grabber",
+		Flags:       &flags,
+		AutoHelp:    true,
+		AutoVersion: true,
+		AnyError:    true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set Debug
+	if cmd.FlagArgs("Debug") != nil {
+		debug = true
+		fmt.Println("WARNING: DEBUG ENABLED!")
+	}
+
+	// Grab command
+	if cmd.FlagArgs("Grab") != nil {
+		args := strings.Trim(fmt.Sprintf("%s\n", strings.TrimRight(strings.TrimLeft(fmt.Sprintf("%v",
+			cmd.FlagArgs("Grab")[1:]), "["), "]")), "\n")
+		grabSub(args)
+	}
+}
+
+func grabSub(args string) {
 	c := colly.NewCollector(
 		//Allowed domains
 		colly.AllowedDomains("www.subdivx.com", "subdivx.com"),
@@ -44,11 +85,12 @@ func main() {
 		log.Println("Visiting", r.URL.String())
 	})
 
-	/*
-		c.OnResponse(func(r *colly.Response) {
+	c.OnResponse(func(r *colly.Response) {
+		if debug {
 			log.Println(string(r.Body))
-		})
-	*/
+		}
+	})
+
 	c.OnHTML("div#buscador_detalle", func(e *colly.HTMLElement) {
 		text := e.ChildText("#buscador_detalle_sub")
 		link := e.ChildAttr("#buscador_detalle_sub_datos > a[href*='bajar']", "href")
@@ -57,7 +99,7 @@ func main() {
 
 		subs = append(subs, psub)
 	})
-	c.Visit(generateURL())
+	c.Visit(generateURL(args))
 	c.Wait()
 	fmt.Printf("%# v", pretty.Formatter(subs))
 }
